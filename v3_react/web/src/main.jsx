b3eaed9e51cd62af115aaaddcf0a082798b62f98
OpenSorcerer20 <opensorcerer20@gmail.com>
2026-01-01 04:38:23 -0600

    v3: initial working react drag drop

---
+++ v3_react/web/src/main.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+import React from 'react';
+
+import { createRoot } from 'react-dom/client';
+
+import App from './App.jsx';
+
+const rootEl = document.getElementById('root');
+createRoot(rootEl).render(<App />);
